ff3a0d42b717029c82c295be2df2e37decffc843
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-06-08 16:18:20 +0300

    added class User

--- src/main/java/com/gv/VG/security/models/User.java
+++ src/main/java/com/gv/VG/security/models/User.java
@@ -3,15 +3,14 @@ package com.gv.VG.security.models;
 public class User {
 
     private String login;
-
     private String password;
-
-    public User(){}
 
     public User(String login, String password) {
         this.login = login;
         this.password = password;
     }
+
+    public User() {}
 
     public String getLogin() {
         return login;
