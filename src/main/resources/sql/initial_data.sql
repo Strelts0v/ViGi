c5863c076462ce116f5aa2c1fdb7fe7a756b91d1
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-06-09 12:31:56 +0300

    added sql scripts with table and initial data for security

---
+++ src/main/resources/sql/initial_data.sql
@@ -0,0 +1,5 @@
+use vg;
+
+insert into users(email, password, role_mask) values
+("gleb.streltsov.4by@gmail.com", "f0ac2325b6bd0302bcd8862ca5852f99dcfe5a91", 11),
+("vi.veronikasanko@gmail.com", "f0ac2325b6bd0302bcd8862ca5852f99dcfe5a91", 11);
