941f0de3916051b9b5061a0a0eee1e9b428bb368
gleb.streltsov <gleb.streltsov.4by@gmail.com>
2017-07-22 23:25:28 +0300

    update ProfileController class

--- src/main/java/com/gv/VG/controllers/ProfileController.java
+++ src/main/java/com/gv/VG/controllers/ProfileController.java
@@ -1,5 +1,10 @@
 package com.gv.VG.controllers;
 
+import com.gv.VG.dao.interfaces.ProfileDao;
+import com.gv.VG.entities.Profile;
+import com.gv.VG.services.interfaces.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +14,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/profile")
 public class ProfileController {
 
+    @Autowired
+    private ProfileDao profileDao;
+
+    @Autowired
+    private UserService service;
+
     @RequestMapping(method = RequestMethod.GET)
     public String getProfilePage(Model model){
+
+        UserDetails details = (UserDetails)org.springframework.security.core.context.
+                SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Profile profile = profileDao.getProfileByEmail(details.getUsername());
+        model.addAttribute("profile", profile);
         return "/views/profile";
     }
 }
